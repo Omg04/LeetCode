ce2e50137909be8524350b709f3ae3ded6b4f4bf
Omg04 <ng3252873@gmail.com>
2026-06-26 02:43:02 +0530

    Add 'merge nodes in between zeros' to difficulty map

--- .github/scripts/generate_readme.py
+++ .github/scripts/generate_readme.py
@@ -52,6 +52,7 @@ DIFFICULTY_MAP = {
     "remove nodes from linked list":                  ("Medium", "2487"),
     "delete nodes from linked list present in array": ("Medium", "3217"),
     "insert greatest common divisors in linked list": ("Medium", "2807"),
+    "merge nodes in between zeros":                   ("Medium", "2181"),
 }
 
 DIFFICULTY_MAP = {normalize(k): v for k, v in DIFFICULTY_MAP.items()}
